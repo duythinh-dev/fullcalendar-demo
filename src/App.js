23af8a5d35bad572ff597401a4aa4d7ea17d2751
duythinh <thinhogg11@gmail.com>
2024-01-08 17:54:54 +0700

    upload source to git

---
+++ src/App.js
@@ -0,0 +1,226 @@
+import React, { useEffect, useRef, useState } from "react";
+// import { formatDate } from '@fullcalendar/core'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import "./App.css";
+import {
+  INITIAL_EVENTS,
+  createEventId,
+  getFirstAndLastDay,
+  randomColor,
+} from "./helper";
+
+function App() {
+  const fullcalendarRef = useRef();
+
+  const { firstDay, lastDay } = getFirstAndLastDay(new Date(), "dayGridMonth");
+
+  const [currenEvent, setCurrenEvent] = useState(INITIAL_EVENTS);
+  const [focusDate, setFocusDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(firstDay);
+  const [endDate, setEndDate] = useState(lastDay);
+  const [view, setView] = useState(null);
+
+  const handleChangeEvent = (events) => {
+    const event = events.event;
+    const idChanged = event.id;
+    const eventsData = currenEvent.map((e) => {
+      if (e.id === idChanged) {
+        return {
+          id: event.id,
+          title: event.title,
+          start: event.start,
+          end: event.end,
+          color: randomColor(),
+          allDay: event.allDay,
+        };
+      }
+      return e;
+    });
+    setCurrenEvent(eventsData);
+  };
+  const handleRemoveEvent = (clickInfo) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
+  const handleAddEvent = (selectInfo) => {
+    let title = window.prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+    if (title) {
+      setCurrenEvent([
+        ...currenEvent,
+        {
+          id: createEventId(),
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay,
+          color: randomColor(),
+        },
+      ]);
+    }
+  };
+
+  const handleChangeTime = (date) => {
+    let calendarApi = fullcalendarRef.current.getApi();
+    if (calendarApi) {
+      calendarApi.gotoDate(date);
+      setFocusDate(date);
+      handleChangeAllTime(view, date);
+    }
+  };
+  const handleChangeView = (e) => {
+    const viewType = e.view.type;
+    handleChangeAllTime(viewType, focusDate);
+    setView(viewType);
+  };
+
+  const handleChangeAllTime = (v, fcd) => {
+    if (v !== "timeGridDay") {
+      const objDate = getFirstAndLastDay(new Date(fcd), v);
+      setStartDate(objDate.firstDay);
+      setEndDate(objDate.lastDay);
+    } else {
+      setStartDate(fcd);
+      setEndDate(fcd);
+    }
+  };
+
+  const handleEventRecieve = (info) => {
+    console.log("info", info);
+    var checkbox = document.getElementById("drop-remove");
+    setCurrenEvent([
+      ...currenEvent,
+      {
+        id: createEventId(),
+        title: info.draggedEl.textContent,
+        start: info.date,
+        allDay: !!info.allDay,
+        color: randomColor(),
+      },
+    ]);
+    if (checkbox.checked) {
+      info.draggedEl.style.display = "none";
+    }
+  };
+
+  useEffect(() => {
+    let draggableEl = document.getElementById("external-events");
+    new Draggable(draggableEl, {
+      itemSelector: ".fc-event",
+      eventData: function (eventEl) {
+        console.log("eventEl", eventEl);
+        return {
+          title: eventEl.innerText,
+          create: false,
+        };
+      },
+    });
+  }, []);
+
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        padding: "30px",
+        gap: "10px",
+      }}
+    >
+      <div
+        style={{
+          width: "20%",
+        }}
+      >
+        <div id="external-events">
+          <p>
+            <strong>Draggable Events</strong>
+          </p>
+
+          <div className="fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event">
+            <div className="fc-event-main">My Event 1</div>
+          </div>
+          <div className="fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event">
+            <div className="fc-event-main">My Event 2</div>
+          </div>
+          <div className="fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event">
+            <div className="fc-event-main">My Event 3</div>
+          </div>
+          <div className="fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event">
+            <div className="fc-event-main">My Event 4</div>
+          </div>
+          <div className="fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event">
+            <div className="fc-event-main">My Event 5</div>
+          </div>
+
+          <p>
+            <input type="checkbox" id="drop-remove" />
+            <label htmlFor="drop-remove">remove after drop</label>
+          </p>
+        </div>
+        <div>
+          Date:{" "}
+          <DatePicker
+            placeholderText="Time"
+            selected={startDate}
+            onChange={handleChangeTime}
+            startDate={startDate}
+            endDate={endDate}
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          width: "80%",
+        }}
+      >
+        <FullCalendar
+          initialView="dayGridMonth"
+          headerToolbar={{
+            left: "prev,next",
+            center: "title",
+            right: "dayGridMonth,timeGridTwoWeek,timeGridWeek,timeGridDay",
+          }}
+          views={{
+            timeGridTwoWeek: {
+              type: "timeGrid",
+              duration: { days: 14 },
+              buttonText: "2 Week",
+            },
+          }}
+          ref={fullcalendarRef}
+          themeSystem="Simplex"
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          droppable
+          // initialEvents={INITIAL_EVENTS}
+          events={currenEvent}
+          editable
+          selectable
+          // selectMirror
+          dayMaxEvents
+          weekends
+          eventChange={handleChangeEvent}
+          eventDrop={handleChangeEvent}
+          handleEventClick={handleRemoveEvent}
+          select={handleAddEvent}
+          datesSet={handleChangeView}
+          drop={handleEventRecieve}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/helper/index.js
@@ -0,0 +1,67 @@
+let eventGuid = 0;
+
+const arrColor = [
+  "#2C3333",
+  "#2E4F4F",
+  "#0E8388",
+  "#CBE4DE",
+  "#451952",
+  "#662549",
+  "#AE445A",
+  "#F39F5A",
+];
+
+function randomDate(start, end) {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+}
+
+export const INITIAL_EVENTS = Array.apply(null, { length: 100 }).map(
+  (_, index) => {
+    return {
+      id: createEventId(),
+      title: `Event ${index}`,
+      start: randomDate(new Date(2024, 1, 1), new Date()),
+      color: randomColor(),
+      allDay: index % 5 === 0 ? true : false,
+    };
+  }
+);
+
+export function createEventId() {
+  return String(eventGuid++);
+}
+
+export function randomColor() {
+  const idxRandom = Math.floor(Math.random() * arrColor.length);
+  return arrColor[idxRandom];
+}
+
+export function getFirstAndLastDay(day, type) {
+  const date = new Date(day);
+  let firstDay;
+  let lastDay;
+  if (type === "timeGridWeek" || type === "timeGridTwoWeek") {
+    const first = date.getDate() - date.getDay(); // First day is the day of the month - the day of the week
+    const last = first + (type === "timeGridTwoWeek" ? 13 : 6); // last day is the first day + 6
+    console.log(first, last, type);
+    firstDay = new Date(date.setDate(first));
+    lastDay = new Date(date.setDate(last));
+  } else {
+    firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  }
+
+  return {
+    firstDay,
+    lastDay,
+  };
+}
+
+export function getSunday(d) {
+  d = new Date(d);
+  var day = d.getDay(),
+    diff = d.getDate() - day + (day == 0 ? -6 : 0);
+  return new Date(d.setDate(diff));
+}
